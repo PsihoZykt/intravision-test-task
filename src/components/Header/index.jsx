a1f51267cb34027d120d5291abe3f1f28b208726
psihozykt <psihozykt@gmail.com>
2022-02-04 12:21:49 +0900

    initial setup. Added markup of navbar, beginning of main page and topmenu

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import analytics from '../../assets/img/navbar/analytics.png'
+import clients from '../../assets/img/navbar/clients.png'
+import employees from '../../assets/img/navbar/employees.png'
+import knowledge_base from '../../assets/img/navbar/knowledge_base.png'
+import logo from '../../assets/img/navbar/logo.png'
+import settings from '../../assets/img/navbar/settings.png'
+import tasks from '../../assets/img/navbar/tasks.png'
+import s from './index.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={s.navbar}>
+            <div className={s.navbarItem} >
+                <img src={logo} alt="Intravision logo, looks like bottom-arrow"/></div>
+            <div className={s.navbarItem}>
+                <img src={knowledge_base} alt="A book"/>
+            <div> База знаний</div>
+            </div>
+            <div className={s.navbarItem}>
+                <img src={tasks} alt="A paper"/>
+                <div> Заявки</div>
+            </div>
+            <div className={s.navbarItem}>
+                <img src={employees} alt="three people"/>
+                <div> Сотрудники </div>
+            </div>
+            <div className={s.navbarItem}>
+                <img src={clients} alt="some buildings"/>
+                <div> Клиенты </div>
+            </div>
+            <div className={s.navbarItem}>
+                <img src={analytics} alt="screen with some dashboards on it"/>
+                <div> Активы</div>
+            </div>
+            <div className={s.navbarItem}>
+                <img src={settings} alt="cogwheel"/>
+                <div> Настройки </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
